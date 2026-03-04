c7fc422df56fce595d1983a0e38a81a2b84981ef
evansowa <evan.sowa728@gmail.com>
2026-03-03 20:46:41 -0500

    Evan Submission

---
+++ Unnamed_Project/Assets/Scripts/NewMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement3D : MonoBehaviour
+{
+    public float speed = 6f;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
